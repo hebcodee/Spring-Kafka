37ffeceaf06371b875e4b1b930cc59b825161ca6
hebcodee <herberth.ferreira6@gmail.com>
2026-03-17 17:31:20 -0300

    feat: kafka producer remover-agencia-channel

--- src/main/java/br/com/alura/service/SituacaoCadastralService.java
+++ src/main/java/br/com/alura/service/SituacaoCadastralService.java
@@ -3,8 +3,11 @@ package br.com.alura.service;
 import br.com.alura.domain.Agencia;
 import br.com.alura.domain.audit.Audit;
 import br.com.alura.repository.SituacaoCadastralRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
@@ -15,11 +18,19 @@ public class SituacaoCadastralService {
     private final SituacaoCadastralRepository situacaoCadastralRepository;
 
     private final Emitter<Audit> emitter;
+    private final MutinyEmitter<String> mutinyEmitter;
+    private final ObjectMapper objectMapper;
 
 
-    public SituacaoCadastralService(SituacaoCadastralRepository situacaoCadastralRepository, @Channel("notificacoes") Emitter<Audit> emitter) {
+    public SituacaoCadastralService(
+            SituacaoCadastralRepository situacaoCadastralRepository,
+            @Channel("notificacoes") Emitter<Audit> emitter,
+            @Channel("remover-agencia-channel") MutinyEmitter<String> mutinyEmitter
+    ) {
         this.situacaoCadastralRepository = situacaoCadastralRepository;
         this.emitter = emitter;
+        this.mutinyEmitter = mutinyEmitter;
+        this.objectMapper = new ObjectMapper();
     }
 
     @WithTransaction
@@ -30,6 +41,16 @@ public class SituacaoCadastralService {
                 .onItem().invoke(a -> {
                     // envio "fire-and-forget"
                     emitter.send(new Audit(agencia.getId(), agencia.getCnpj(), agencia.getSituacaoCadastral()));
+                })
+                .call (() -> {
+                    try{
+                        if (agencia.getSituacaoCadastral().equals("INATIVO")) {
+                            return mutinyEmitter.send(objectMapper.writeValueAsString(agencia));
+                        }
+                        return Uni.createFrom().voidItem();
+                    } catch (JsonProcessingException e) {
+                        return Uni.createFrom().failure(e);
+                    }
                 })
                 .replaceWithVoid();
     }
